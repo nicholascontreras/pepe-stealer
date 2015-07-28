d44785ddd260e9834ed43f27e2d5afdb6a40453f
aklreaxmer <amkramer2010@yahoo.com>
2015-07-27 20:35:53 -0700

    add stuf

--- game.js
+++ game.js
@@ -2,180 +2,195 @@ Crafty.init(Crafty.viewport.width,Crafty.viewport.height, document.getElementByI
 var assets = {
 	"sprites": {
 		
-		"(1).png": {
+		"img/(1).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe1: [0,0]
 			}
 		},
-		"(2).png": {
+		"img/(2).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe2: [0,0]
 			}
 		},
-		"(3).png": {
+		"img/(3).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe3: [0,0]
 			}
 		},
-		"(4).png": {
+		"img/(4).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe4: [0,0]
 			}
 		},
-		"(5).png": {
+		"img/(5).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe5: [0,0]
 			}
 		},
-		"(6).png": {
+		"img/(6).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe6: [0,0]
 			}
 		},
-		"(7).png": {
+		"img/(7).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe7: [0,0]
 			}
 		},
-		"(8).png": {
+		"img/(8).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe8: [0,0]
 			}
 		},
-		"(9).png": {
+		"img/(9).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe9: [0,0]
 			}
 		},
-		"(10).png": {
+		"img/(10).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe10: [0,0]
 			}
 		},
-		"(11).png": {
+		"img/(11).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe11: [0,0]
 			}
 		},
-		"(12).png": {
+		"img/(12).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe12: [0,0]
 			}
 		},
-		"(13).png": {
+		"img/(13).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe13: [0,0]
 			}
 		},
-		"(14).png": {
+		"img/(14).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe14: [0,0]
 			}
 		},
-		"(15).png": {
+		"img/(15).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe15: [0,0]
 			}
 		},
-		"(16).png": {
+		"img/(16).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe16: [0,0]
 			}
 		},
-		"(17).png": {
+		"img/(17).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe17: [0,0]
 			}
 		},
-		"(18).png": {
+		"img/(18).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe18: [0,0]
 			}
 		},
-		"(19).png": {
+		"img/(19).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe19: [0,0]
 			}
 		},
-		"(20).png": {
+		"img/(20).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe20: [0,0]
 			}
 		},
-		"(21).png": {
+		"img/(21).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe21: [0,0]
 			}
 		},
-		"(22).png": {
+		"img/(22).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe22: [0,0]
 			}
 		},
-		"(23).png": {
+		"img/(23).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe23: [0,0]
 			}
 		},
-		"(24).png": {
+		"img/(24).png": {
 			tile: 375,
 			tileh: 360,
 			map: {
 				pepe24: [0,0]
 			}
+		},
+		"img/robber.png": {
+			tile: 56,
+			tileh: 56,
+			map: {
+				robber: [0,0]
+			}
 		}
+		
 
-	}
-};
+	}};
+
 Crafty.load(assets, function(){});
+
 var pepeCount = 0;
 var manualRate = 1;
+var clickedThief = false;
+var thiefUnlocked = false;
+
+
+var thiefCount = 0, shrekCount=0, opCount=0, cageCount=0, tysonCount=0, ninegagCount=0, redditCount=0, fourchanCount=0;
+var esports = false, dewritos=false, gnome=false, secrets=false;
 newPepe();
 
 Crafty.e('2D, DOM, Color').attr({
@@ -186,12 +201,12 @@ Crafty.e('2D, DOM, Color').attr({
 	"border-radius": "20px"
 })
 Crafty.e('2D, DOM, Text').attr({
-	x: Crafty.viewport.width-500, y:0,
-	w:50
+	x: Crafty.viewport.width-(Crafty.viewport.width/4), y:0,
+	w:(Crafty.viewport.width/3)
 }).text("SHOPE").textFont({"size": "32px", "family": "Comic Sans MS"})
 Crafty.e('2D, DOM, Image').attr({
 	x: Crafty.viewport.width/2-600, y: 0,
-}).image("title.png")
+}).image("img/title.png")
 
 var collectionText = Crafty.e('2D, DOM, Text')
   .attr({
@@ -219,17 +234,14 @@ var collectionText = Crafty.e('2D, DOM, Text')
  	family: 'Comic Sans MS'
  });
 
-
-Crafty.e("2D, DOM, Text").attr({
-	x: Crafty.viewport.width-760,
+Crafty.c("shop-item", {});
+Crafty.c("thief", {});
+Crafty.e("2D, DOM, shop-item, thief").attr({
+	x: Crafty.viewport.width-(Crafty.viewport.width/4)-20,
 	y:70,
-	w:200
-}).text("auto pepe thief").textFont({
-	"family": "Comic Sans MS",
-	"size": "18px"
+	w:150,
+	h:130
 })
-
-
 
 
 function stealPepe (pepe) {
@@ -264,6 +276,10 @@ function stealPepe (pepe) {
 				
 			}	
 	})
+
+	if(pepeCount >= 30)
+		unlockThief();
+
 	
 }
 
@@ -272,5 +288,45 @@ function newPepe() {
 	.attr({x: Crafty.viewport.width/2-500, y: 200, w: 275, h: 260})
 	.bind('Click', function(MouseEvent){
   	stealPepe(this);
+
 });
 }
+function unlockThief() {
+
+	$(".thief").css("background", "#99FFFF");
+	$(".thief").on("click", function(){
+		addThief();
+	})
+	clickedThief = false;
+
+}	
+var thiefOffset = 0;
+function addThief() {
+	if(pepeCount >= 30 && clickedThief == false)
+	{
+		pepeCount -= 30;
+		clickedThief = true;
+		if(pepeCount <= 30)
+		{
+			$(".thief").css("background", "#63C914");
+			$(".thief").on("click", function(){
+			
+			})
+		}
+
+		Crafty.e("2D, DOM, robber, Tween")
+		.attr({x: Crafty.viewport.w/2-500, y: 200+thiefOffset})
+		.tween({x: 1000}, 1000);
+		thiefOffset += 60;
+	}
+
+}
+	
+// setInterval(autoPepes, 1000);
+// function autoPepes() {
+// 	Crafty.e("2D", "DOM", "pepe" + Math.floor((Math.random() * 24) + 1), "Mouse")
+// 	.attr({x: Crafty.viewport.width/2-500, y: 200, w: 275, h: 260})
+// 	.bind('Click', function(MouseEvent){
+//   	stealPepe(this);
+// 	})
+// }
